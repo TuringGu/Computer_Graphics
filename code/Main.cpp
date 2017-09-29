43e10fcbd60184ee2f6444196b140226da0cc2cc
Idea <gulintao850366208@gmail.com>
2017-09-29 21:07:35 +0800

    Description add

--- code/Main.cpp
+++ code/Main.cpp
@@ -7,7 +7,7 @@ void main()
 {
 	int x1 = 100, y1 = 100, x2 = 500, y2 = 300;
 	initgraph(640, 480);
-	LineDDA(x1, y1, x2, y2, YELLOW);
+	LineDDA(x1, y1, x2, y2, YELLOW);// draw a straight line
 	getch();
 	closegraph();
 }
