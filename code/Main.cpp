28cf75a2561b722100ea5cd1385f93f7a64adec1
Idea <gulintao850366208@gmail.com>
2017-09-29 21:04:42 +0800

    1
    1

---
+++ code/Main.cpp
@@ -0,0 +1,13 @@
+// Test
+
+#include "stdafx.h"
+#include "LineDDA.h"
+
+void main()
+{
+	int x1 = 100, y1 = 100, x2 = 500, y2 = 300;
+	initgraph(640, 480);
+	LineDDA(x1, y1, x2, y2, YELLOW);
+	getch();
+	closegraph();
+}
